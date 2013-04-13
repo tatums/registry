48eac42ae87eb3768acc41ddf8ed38629f5528b1
tatums <tatum@ashlandstudios.com>
2013-04-13 10:53:01 -0500

    add version to uploader

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -20,6 +20,11 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  version :thumb do
+    process :resize_to_fill => [200, 120]
+  end
+
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
