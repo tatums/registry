b150bf4a27ffef009ef50d5fac60aefe09a6d576
tatums <tatum@ashlandstudios.com>
2013-04-21 09:38:58 -0500

    setup login via local credentials

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,15 +4,39 @@ class SessionsController < ApplicationController
   end
 
   def create
-    auth = request.env["omniauth.auth"]
-    user = User.find_by(provider: auth["provider"], uid: auth["uid"]) || User.create_with_omniauth(auth)
-    session[:user_id] = user.id
-    redirect_to root_url
+    user = find_user(request.env["omniauth.auth"])
+    if user
+      session[:user_id] = user.id
+      redirect_to root_url
+    else
+      flash.now[:alert] = "Invalid email or password"
+      render "new"
+    end
   end
+
+
+  # def create
+  #   auth = request.env["omniauth.auth"]
+  #   user = User.find_by(provider: auth["provider"], uid: auth["uid"]) || User.create_with_omniauth(auth)
+  #   session[:user_id] = user.id
+  #   redirect_to root_url
+  # end
 
   def destroy
     session[:user_id] = nil
     redirect_to login_path
   end
+
+private
+
+  def find_user(auth)
+    if auth
+      User.find_by(provider: auth["provider"], uid: auth["uid"]) || User.create_with_omniauth(auth)
+    else
+      User.find_by(email: params[:email])
+    end
+  end
+
+
 
 end
