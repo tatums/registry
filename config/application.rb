b03ad27b81b9abc9fbfe9d175fe25c6a9bb5249e
tatums <tatum@ashlandstudios.com>
2013-05-06 19:17:28 -0500

    .

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,6 @@ module Registry
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.enabled = false
+
   end
 end
